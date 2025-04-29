1e5561c8692e5466fb6b2601d105acbaa70b601b
Jorge Espinosa <jyer94@gmail.com>
2025-04-29 02:13:41 -0600

    working: mensaje de los novios

--- src/components/message-from-bride/MessageFromBride.jsx
+++ src/components/message-from-bride/MessageFromBride.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './message-from-bride.scss';
 
 export const MessageFromBride = () => {
     return (
         <div className="message-from-bride message-from-bride__container">
-            <div>
+            <img src="./img/flower.png" className="message-from-bride__flower-top" alt="flower" />
+            <div className="message-from-bride__title">
                 Mensaje de los novios
             </div>
-            <div>
+            <div className="message-from-bride__desc">
                 "Hay momentos que se atesoran en nuestro corazón para siempre; por esta razón, queremos compartir este momento especial contigo"
             </div>
         </div>
